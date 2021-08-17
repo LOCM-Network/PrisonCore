028f1434021f4f6d1219cf6195389897b099020d
ZzKino <clonevcc1@gmail.com>
2021-08-17 18:37:21 +0700

    lombok test jdk

--- src/main/java/com/paradox/core/mines/obj/MineBlock.java
+++ src/main/java/com/paradox/core/mines/obj/MineBlock.java
@@ -1,24 +1,17 @@
 package com.paradox.core.mines.obj;
 
 import cn.nukkit.block.Block;
+import lombok.Getter;
 
 public class MineBlock {
 
-	private Block block;
-	private int chance;
+	@Getter
+	private final Block block;
+	@Getter
+	private final int chance;
 
 	public MineBlock(Block block, int chance) {
-		super();
 		this.block = block;
 		this.chance = chance;
 	}
-
-	public Block getBlock() {
-		return block;
-	}
-
-	public int getChance() {
-		return chance;
-	}
-
 }
