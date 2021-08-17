9c929f038b26260e7608d953a4b4e174b9dd704f
Phuongaz <42109045+ZzKino@users.noreply.github.com>
2021-08-17 15:11:30 +0700

    Oops

--- src/main/java/com/paradox/core/Loader.java
+++ src/main/java/com/paradox/core/Loader.java
@@ -62,8 +62,6 @@ public class Loader extends PluginBase {
 
 	@Override
 	public void onEnable() {
-		Server.getInstance().getNetWork().setName(TextFormat.colorize("&l&eＬＯＣＭ"));
-		Server.getInstance().getNetWork().setSubName(TextFormat.colorize("&l&bＰＲＩＳＯＮ"));
 		getDataFolder().mkdirs();
 		registerfiles();
 		GeneralUtils.setupWorthFile();
